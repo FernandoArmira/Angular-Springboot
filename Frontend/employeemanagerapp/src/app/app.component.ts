8465a3da6e14186b12c8e2c643b89d99b9458700
FernandoArmira <2864934620101@ingenieria.usac.edu.gt>
2022-08-23 23:51:24 -0600

    Agregar y editar empleados

--- Frontend/employeemanagerapp/src/app/app.component.ts
+++ Frontend/employeemanagerapp/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Employee } from './employee';
 import { EmployeeService } from './employee.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +11,7 @@ import { EmployeeService } from './employee.service';
 })
 export class AppComponent implements OnInit{
   public employees: Employee[];
+  public editEmployee!: Employee | null;
 
   constructor(private employeeService:EmployeeService){
     this.employees = [];
@@ -28,5 +30,53 @@ export class AppComponent implements OnInit{
         alert(error.message)
       }
     );
+  }
+  
+  public onAddEmloyee(addForm: NgForm): void {
+    document.getElementById('add-employee-form')?.click();
+    this.employeeService.addEmployee(addForm.value).subscribe(
+      (response: Employee) => {
+        console.log(response);
+        this.getEmployees();
+        addForm.reset();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+        addForm.reset();
+      }
+    );
+  }
+
+  public onUpdateEmloyee(employee: Employee): void {
+    this.employeeService.updateEmployee(employee).subscribe(
+      (response: Employee) => {
+        console.log(response);
+        this.getEmployees();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  public onOpenModal(employee: Employee| null, mode: string): void{
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle','modal');
+    if(mode === 'add'){
+      button.setAttribute('data-target','#addEmployeeModal')
+    }
+    if(mode === 'edit'){
+      this.editEmployee = employee;
+      button.setAttribute('data-target','#updateEmployeeModal')
+    }
+    if(mode === 'delete'){
+      button.setAttribute('data-target','#deleteEmployeeModal')
+    }
+    container?.appendChild(button);
+    button.click();
+
   }
 }
